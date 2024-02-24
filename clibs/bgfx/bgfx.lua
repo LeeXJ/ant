6935c4a1a139f63d6ca0f9200420f834aad3993e
LeeXJ <872846303@QQ.COM>
2024-02-24 04:41:02 +0000

    t

--- clibs/bgfx/bgfx.lua
+++ clibs/bgfx/bgfx.lua
@@ -18,3 +18,13 @@ lm:copy "copy_bgfx_shader" {
         lm.AntDir .. "/pkg/ant.resources/shaders/shaderlib.sh",
     }
 }
+
+-- 这段 Lua 代码用于构建一个项目，并进行以下操作：
+
+-- 1. 设置了 `lm.BgfxDir`、`lm.BxDir` 和 `lm.BimgDir` 分别指向 bgfx、bx 和 bimg 库的目录路径。
+-- 2. 导入了 bgfx 库的构建配置文件，通过 `lm:import` 函数引入了 `"luamake.AntDir/3rd/bgfx.luamake/use.lua"` 文件。
+-- 3. 声明了一个拷贝任务 `copy_bgfx_shader`，用于将指定的 shader 脚本文件从 bgfx 目录复制到项目目录中的指定路径。
+-- 4. 指定了拷贝任务的输入文件列表，包括了 `bgfx_shader.sh`、`bgfx_compute.sh` 和 `shaderlib.sh` 三个文件。
+-- 5. 指定了拷贝任务的输出文件列表，分别指定了复制到项目目录中的目标路径。
+
+-- 综上所述，该代码片段实现了将 bgfx 库的 shader 脚本文件复制到项目目录的指定位置的功能。
--- clibs/bgfx/luabgfx.h
+++ clibs/bgfx/luabgfx.h
@@ -60,3 +60,13 @@ lua_setiuservalue(lua_State *L, int idx, int n) {
 #endif
 
 #endif
+
+// 这段 C 头文件代码实现了以下功能：
+
+// 1. 定义了一个枚举 `BGFX_HANDLE`，列出了一系列 bgfx 句柄的类型，如程序、着色器、顶点缓冲等。
+// 2. 定义了几个宏来处理 bgfx 句柄，例如 `BGFX_LUAHANDLE` 用于构造句柄，`BGFX_LUAHANDLE_ID` 用于检查句柄类型。
+// 3. 实现了一个辅助函数 `check_handle_type`，用于检查句柄类型是否正确。
+// 4. 声明了一个结构体 `memory`，用于表示内存块，包括了指向数据的指针、大小、引用计数和是否为常量的标志。
+// 5. 对于 Lua 版本号低于 5.4，提供了一些兼容性的宏定义，用于处理新版 Lua 中的 userdata 的操作。
+
+// 综上所述，该头文件主要用于定义了一些常量、宏和结构体，以支持在 Lua 中与 bgfx 库进行交互时所需的功能。
